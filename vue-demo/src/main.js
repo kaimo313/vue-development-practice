d206e223f00353092a1c22141fa3893dc178133d
kaimo <kaimo313@foxmail.com>
2022-09-07 01:08:59 +0800

    feat：demo9：合理应用计算属性和侦听器

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import App from './views/demo8/index.vue'
+import antd from "ant-design-vue";
+
+import App from './views/demo9/index.vue'
+import "ant-design-vue/dist/antd.css";
 
 Vue.config.productionTip = false
+
+Vue.use(antd);
 
 new Vue({
   render: h => h(App),
