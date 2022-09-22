3d21b5aef0ca509cbeacc51444e9158aab69658f
kaimo313 <kaimo313@foxmail.com>
2022-09-22 12:09:36 +0800

    feat: 如何自定义Webpack和Babel配置

--- ant-design-vue-pro/src/main.js
+++ ant-design-vue-pro/src/main.js
@@ -3,7 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// import Antd from "ant-design-vue";
+// // 需要自定义样式主题，这里使用less
+// import "ant-design-vue/dist/antd.less";
+
+// import Button from "ant-design-vue/lib/button";
+// import 'ant-design-vue/lib/button/style';
+
+import { Button } from "ant-design-vue";
+
 Vue.config.productionTip = false;
+
+// Vue.use(Antd);
+Vue.use(Button);
 
 new Vue({
   router,
