30247d5c50f10bc08f61f726fbd0be4f4e14622c
kaimo <kaimo313@foxmail.com>
2022-09-27 00:23:11 +0800

    fix：修复安装依赖以及jsx写法问题导致出来效果

--- ant-design-vue-pro/src/utils/request.js
+++ ant-design-vue-pro/src/utils/request.js
@@ -7,12 +7,16 @@ function request(options) {
     }).catch((error) => {
         const { response: { status, statusText }} = error;
         notification.error({
-            // message: h => {
+            // message: h => (
             //     <div>
             //         请求错误 <span style="color: red">{status}</span>：{options.url}
             //     </div>
-            // },
-            message: status,
+            // ),
+            message: h => {
+                return <div>
+                    请求错误 <span style="color: red">{status}</span>：{options.url}
+                </div>
+            },
             description: statusText
         });
         return Promise.reject(error);
